1224cd0ee7f83b3172a5af26b9cf18ac28a7f28d
nwilliams <nwilliams@pcienergysolutions.com>
2023-03-02 17:17:04 -0600

    Use the batch file to add python to path

--- 1-RUN THIS FIRST.bat
+++ 1-RUN THIS FIRST.bat
@@ -1,4 +1,6 @@
 @echo off
+FOR /f %%p in ('where python') do SET PYTHONPATH=%%p
+ECHO %PYTHONPATH%
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 pip install -r requirements.txt
